e7f05511da651ac9d7f0ebc5c5c68dd150bb8ba3
Thomas Gonzalez <gonzalez.thomas.tg+github@gmail.com>
2021-09-13 11:15:43 +0200

    WIP: get scaling from @knyghtryda

--- lib/android.dart
+++ lib/android.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:flutter_launcher_icons/utils.dart';
 import 'package:flutter_launcher_icons/xml_templates.dart' as xml_template;
 import 'package:image/image.dart';
@@ -68,24 +69,71 @@ bool isAndroidIconNameCorrectFormat(String iconName) {
   return true;
 }
 
+/// Rescales image by creating a different sized canvas and copying image centered onto the new canvas
+Image rescaleImage(Image image, double scaleFactor, {int fillColor = 0}) {
+  printStatus('Rescaling icon by $scaleFactor');
+  final int scaledDimension =
+      (max(image.width, image.height) * 1 / scaleFactor).floor();
+  if (image.width != image.height) {
+    printStatus(
+        'Foreground image is not square!  Scaled canvas will be made square '
+        'to meet Adaptive Icon Specifications');
+  }
+  final Image scaledImage = Image(scaledDimension, scaledDimension);
+  scaledImage.fill(fillColor);
+  copyInto(
+    scaledImage,
+    image,
+    dstX: ((scaledDimension - image.width) / 2).floor(),
+    dstY: ((scaledDimension - image.height) / 2).floor(),
+  );
+  return scaledImage;
+}
+
 void createAdaptiveIcons(
     Map<String, dynamic> flutterLauncherIconsConfig, String? flavor) {
   printStatus('Creating adaptive icons Android');
 
-  // Retrieve the necessary Flutter Launcher Icons configuration from the pubspec.yaml file
+  // Retrieve the necessary Flutter Launcher Icons configuration from the yaml file
   final String backgroundConfig =
       flutterLauncherIconsConfig['adaptive_icon_background'];
   final String foregroundImagePath =
       flutterLauncherIconsConfig['adaptive_icon_foreground'];
-  final Image? foregroundImage = decodeImageFile(foregroundImagePath);
+  Image? foregroundImage = decodeImageFile(foregroundImagePath);
   if (foregroundImage == null) {
     return;
+  }
+  final double? foregroundScaleFactor =
+      flutterLauncherIconsConfig['adaptive_icon_foreground_scale_factor'];
+  final String? foregroundScaleFillColor =
+      flutterLauncherIconsConfig['adaptive_icon_foreground_scale_fill_color'];
+
+  final bool rescale = foregroundScaleFactor != null &&
+      foregroundScaleFactor > 0;
+
+  // Scales the foreground image prior to converting to icon.
+  // This is intended for scaling down to match adaptive icon spec
+  if (rescale) {
+    int _getColorFromHex(String hexColor) {
+      //Converts hex string to int
+      hexColor = hexColor.toUpperCase().replaceAll('#', '');
+      return int.parse(hexColor, radix: 16);
+    }
+
+    foregroundImage = rescaleImage(foregroundImage, foregroundScaleFactor,
+        fillColor: foregroundScaleFillColor != null &&
+                foregroundScaleFillColor.isNotEmpty
+            ? _getColorFromHex(foregroundScaleFillColor)
+            : 0);
   }
 
   // Create adaptive icon foreground images
   for (AndroidIconTemplate androidIcon in adaptiveForegroundIcons) {
-    overwriteExistingIcons(androidIcon, foregroundImage,
-        constants.androidAdaptiveForegroundFileName, flavor);
+    overwriteExistingIcons(
+        androidIcon,
+        foregroundImage,
+        constants.androidAdaptiveForegroundFileName,
+        flavor);
   }
 
   // Create adaptive icon background
